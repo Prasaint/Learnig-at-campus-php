b900d711bc80ead23e6958f7c9e7132d34a99a32
Prasaint <prashantkhadkapass@gmail.com>
2025-04-20 15:41:19 +0545

    Push php file

---
+++ index.php
@@ -0,0 +1,22 @@
+<form method="POST" action="index.php">
+<label for="username">Username:</label>
+<input type="text" id="username" name="username" required><br><br>
+
+<label for="email">Email:</label>
+<input type="email" id="email" name="email" required><br><br>
+
+<input type="submit" value="Submit">
+</form>
+<?php
+$username=$_POST['username'];
+$email=$_POST['email'];
+if(!preg_match("/^[a-zA-Z]*$/", $username)){
+    echo "username is not valid.<br/>";
+}
+else if(!filter_var(value: $email,filter: FILTER_VALIDATE_EMAIL)){
+    echo "Invalid email format.<br/>";
+}
+else{
+    echo "Username: ".$username."<br/> and email: ".$email."";
+}
+?>
